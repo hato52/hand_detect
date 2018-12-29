235f4cb5ecf234f5bd9d4baa5d10248886e09775
User <hato.gt@gmail.com>
2018-12-29 18:45:21 +0900

    add process communication

--- gesture_detect.py
+++ gesture_detect.py
@@ -1,12 +1,13 @@
 # coding:utf-8
 ## hand_tracking.py
-## 手の検出を行う
+## ジェスチャの検出を行う
 
 from __future__ import print_function
+import sys
 import numpy as np
 import pyrealsense2 as rs
 import cv2
-import sys
+import win32pipe, win32file
 
 ## 取得する画像サイズ
 IMG_WIDTH = 640
@@ -21,7 +22,7 @@ SAVE_FRAME_SIZE = 5
 ## モーション検出の閾値
 THRESHOLD_X = 10
 THRESHOLD_Y = 10
-THRESHOLD_Z = 0.15
+THRESHOLD_Z = 0.015
 
 ## モーション
 NO_MOTION = 0
@@ -38,16 +39,49 @@ vertical_ary = []
 dist_ary = []
 
 ## カスケード分類器による手の検出を行う
-def detectHandInImage(image, cascade, depth):
-    # 手の検出
-    hands = cascade.detectMultiScale(image, 1.6, 3, 0, (30, 30))
+# def detectHandInImage(image, cascade, depth):
+#     # 手の検出
+#     hands = cascade.detectMultiScale(image, 1.6, 3, 0, (30, 30))
+
+#     nearest = {"x" : 0, "y" : 0, "w" : 0, "h" : 0}
+#     nearest_dist = 100.0
+
+#     if len(hands) == 0: return None, None, None
+
+#     # 検出したオブジェクトの中で最も近いものを判定
+#     for x, y, w, h in hands:
+#         if depth:
+#             tmp = round(depth.get_distance(x + (w / 2), y + (h / 2)), 3)
+#             if nearest_dist > tmp:
+#                 nearest_dist = tmp
+#                 nearest["x"] = x
+#                 nearest["y"] = y
+#                 nearest["w"] = w
+#                 nearest["h"] = h
+                      
+#     # 一番近いものを手として検出
+#     if nearest_dist <= 0.8:
+#         cv2.rectangle(image, (nearest["x"], nearest["y"]), (nearest["x"] + nearest["w"], nearest["y"] + nearest["h"]), (0, 200, 0), 3)
+
+#     x = nearest["x"] + (nearest["w"] / 2)
+#     y = nearest["y"] + (nearest["h"] / 2)
+
+#     return x, y, nearest_dist
+
+
+## HOG-SVM分類器による手の検出を行う
+def detectHand(img, hog, depth):
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # 検出
+    hands, w = hog.detectMultiScale(gray_img, hitThreshold=-1, winStride=(16, 16), padding=(4, 4), scale=1.2, finalThreshold=5)
 
     nearest = {"x" : 0, "y" : 0, "w" : 0, "h" : 0}
     nearest_dist = 100.0
 
     if len(hands) == 0: return None, None, None
 
-    # 検出したオブジェクトの中で最も近いものを判定
+    ## 検出したオブジェクトの中で最も近いものを判定
     for x, y, w, h in hands:
         if depth:
             tmp = round(depth.get_distance(x + (w / 2), y + (h / 2)), 3)
@@ -57,56 +91,15 @@ def detectHandInImage(image, cascade, depth):
                 nearest["y"] = y
                 nearest["w"] = w
                 nearest["h"] = h
-                      
-    # 一番近いものを手として検出
+
     if nearest_dist <= 0.8:
-        cv2.rectangle(image, (nearest["x"], nearest["y"]), (nearest["x"] + nearest["w"], nearest["y"] + nearest["h"]), (0, 200, 0), 3)
+        pad_w, pad_h = int(0.1*nearest["w"]), int(0.1*nearest["h"])
+        cv2.rectangle(img, (nearest["x"]+pad_w, nearest["y"]+pad_h), (nearest["x"]+nearest["w"]-pad_w, nearest["y"]+nearest["h"]-pad_h), (0, 200, 0), 3)
 
     x = nearest["x"] + (nearest["w"] / 2)
     y = nearest["y"] + (nearest["h"] / 2)
 
     return x, y, nearest_dist
-
-
-## HOG-SVM分類器による手の検出を行う
-def detectHand(img, hog, depth):
-    # グレーイメージにしてサイズを縮小する
-    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # gray_img = cv2.GaussianBlur(gray_img, (5,5), 3, 3)
-    # gray_img = cv2.Canny(gray_img, 50, 100)
-
-    # 検出
-    hands, w = hog.detectMultiScale(gray_img, hitThreshold=0, winStride=(16, 16), padding=(4, 4), scale=1.05, finalThreshold=5)
-
-    nearest = {"x" : 0, "y" : 0, "w" : 0, "h" : 0}
-    nearest_dist = 100.0
-
-    #if len(hands) == 0: return None, None, None
-
-    for x, y, w, h in hands:
-        pad_w, pad_h = int(0.1*w), int(0.1*h)
-        cv2.rectangle(img, (x+pad_w, y+pad_h), (x+w-pad_w, y+h-pad_h), (0, 255, 0), 3)
-
-    # 検出したオブジェクトの中で最も近いものを判定
-    # for x, y, w, h in hands:
-    #     if depth:
-    #         tmp = round(depth.get_distance(x + (w / 2), y + (h / 2)), 3)
-    #         if nearest_dist > tmp:
-    #             nearest_dist = tmp
-    #             nearest["x"] = x
-    #             nearest["y"] = y
-    #             nearest["w"] = w
-    #             nearest["h"] = h
-
-    # print(nearest)
-    # if nearest_dist <= 0.8:
-    #     pad_w, pad_h = int(0.1*nearest["w"]), int(0.1*nearest["h"])
-    #     cv2.rectangle(img, (nearest["x"]+pad_w, nearest["y"]+pad_h), (nearest["x"]+nearest["w"]-pad_w, nearest["y"]+nearest["h"]-pad_h), (0, 200, 0), 3)
-
-    # x = nearest["x"] + (nearest["w"] / 2)
-    # y = nearest["y"] + (nearest["h"] / 2)
-
-    #return x, y, nearest_dist
 
 
 ## 画像の背景除去
@@ -146,10 +139,10 @@ def detectHandMoved_X(x):
 
     if motion_flg == SAVE_FRAME_SIZE - 1:
         print("RIGHT")
-        return 1
+        return RIGHT
     elif motion_flg == -(SAVE_FRAME_SIZE - 1):
         print("LEFT")
-        return 2
+        return LEFT
 
     return 0
 
@@ -183,10 +176,10 @@ def detectHandMoved_Y(y):
 
     if motion_flg == SAVE_FRAME_SIZE - 1:
         print("DOWN")
-        return 1
+        return DOWN
     elif motion_flg == -(SAVE_FRAME_SIZE - 1):
         print("UP")
-        return 2
+        return UP
 
     return 0
 
@@ -228,37 +221,37 @@ def detectHandMoved_Z(z):
     return NO_MOTION
 
 
+## 名前付きパイプによるプロセス間通信を行う
+def sendMessageToServer(file_handle, message):
+    if (message == 0): return
+
+    if message == PULL:
+        send_mes = "pull"
+    elif message == PUSH:
+        send_mes = "push"
+    elif message == LEFT:
+        send_mes = "left"
+    elif message == RIGHT:
+        send_mes = "right"
+    elif message == DOWN:
+        send_mes = "down"
+    elif message == UP:
+        send_mes = "up"
+    else:
+        send_mes = None
+
+    try:
+        win32file.WriteFile(file_handle, send_mes)
+    except Exception:
+        print("Failed to send message")
+
+
 def main():
+
     # カスケードファイルの読み込み
-    cascade_file = "aGest.xml"
-    cascade = cv2.CascadeClassifier()
-    cascade.load(cascade_file)
-
-    ## パイプラインの用意
-    pipeline = rs.pipeline()
-
-    ## キャプチャするフレームの設定
-    config = rs.config()
-    config.enable_stream(rs.stream.color, IMG_WIDTH, IMG_HEIGHT, rs.format.bgr8, 30)
-    config.enable_stream(rs.stream.depth, IMG_WIDTH, IMG_HEIGHT, rs.format.z16, 30)
-
-    ## 後処理用のフィルタ設定
-    spa_filter = rs.spatial_filter()    # 画像の平滑化
-    spa_filter.set_option(rs.option.holes_fill, 3)
-
-    tem_filter = rs.temporal_filter()    # 時間的なノイズの低減
-    tem_filter.set_option(rs.option.holes_fill, 1)
-
-    ## ストリーミングの開始
-    profile = pipeline.start(config)
-
-    ## 除去を行う距離を算出
-    depth_sensor = profile.get_device().first_depth_sensor()
-    depth_scale = depth_sensor.get_depth_scale()
-    clipping_distance = CLIPPING_DISTANCE_IN_METERS / depth_scale
-
-    ## プリセットの適用
-    depth_sensor.set_option(rs.option.visual_preset, 2)
+    # cascade_file = "aGest.xml"
+    # cascade = cv2.CascadeClassifier()
+    # cascade.load(cascade_file)
 
     ## HOGDescriptorのインスタンス化
     win_size = (100, 100)       # 検出窓のサイズ
@@ -270,9 +263,49 @@ def main():
     hog = cv2.HOGDescriptor(win_size, block_size, block_stride, cell_size, nbins)
 
     ## SVM分類器にサポートベクトルをセット
-    svm = cv2.ml.SVM_load("hand_edge_svm.xml")
+    svm = cv2.ml.SVM_load("hand_svm.xml")
     primal = svm.getSupportVectors()
     hog.setSVMDetector(np.array(primal))
+
+    ## プロセス間通信用のパイプへ接続する
+    pipe_handle = None
+    try:
+        pipe_handle = win32file.CreateFile(
+            '\\\\.\\pipe\\mypipe',
+            win32file.GENERIC_WRITE,
+            0, 
+            None, 
+            win32file.OPEN_EXISTING, 
+            win32file.FILE_ATTRIBUTE_NORMAL,
+            None)
+    except Exception:
+        print("Failed to connect to pipe")
+
+    ## パイプラインの用意
+    pipeline = rs.pipeline()
+
+    ## キャプチャするフレームの設定
+    config = rs.config()
+    config.enable_stream(rs.stream.color, IMG_WIDTH, IMG_HEIGHT, rs.format.bgr8, 30)
+    config.enable_stream(rs.stream.depth, IMG_WIDTH, IMG_HEIGHT, rs.format.z16, 30)
+
+    ## ストリーミングの開始
+    profile = pipeline.start(config)
+
+    ## 後処理用のフィルタ設定
+    # spa_filter = rs.spatial_filter()    # 画像の平滑化
+    # spa_filter.set_option(rs.option.holes_fill, 3)
+
+    # tem_filter = rs.temporal_filter()    # 時間的なノイズの低減
+    # tem_filter.set_option(rs.option.holes_fill, 1)
+
+    ## 除去を行う距離を算出
+    # depth_sensor = profile.get_device().first_depth_sensor()
+    # depth_scale = depth_sensor.get_depth_scale()
+    # clipping_distance = CLIPPING_DISTANCE_IN_METERS / depth_scale
+
+    ## プリセットの適用
+    # depth_sensor.set_option(rs.option.visual_preset, 2)
 
     try:
         while True:
@@ -298,28 +331,25 @@ def main():
             color_frame = np.asanyarray(color.get_data())
             depth_frame = np.asanyarray(filtered_depth.get_data())
 
-            #color_frame = cv2.resize(color_frame, None, fx=0.5, fy=0.5)
-
             ## 取得した画像の背景除去
             removed = color_frame
             #removed = removeBackground(color_frame, depth_frame, clipping_distance)
 
             ## 手の検出
             #x, y, dist = detectHandInImage(color_frame, cascade, depth)
-            detectHandInImage(removed, cascade, depth)
-            #x, y, dist = detectHand(removed, hog, depth)
-            #detectHand(removed, hog, depth)
+            x, y, dist = detectHand(removed, hog, depth)
  
             ## モーションの検出
-            # message = NO_MOTION
-            # if message == NO_MOTION and x != None:
-            #     message = detectHandMoved_X(x)
-            # if message == NO_MOTION and y != None:
-            #     detectHandMoved_Y(y)
-            # if message == NO_MOTION and dist != None and dist != 0.0:
-            #     detectHandMoved_Z(dist)
+            message = NO_MOTION
+            if message == NO_MOTION and x != None:
+                message = detectHandMoved_X(x)
+            if message == NO_MOTION and y != None:
+                message = detectHandMoved_Y(y)
+            if message == NO_MOTION and dist != None and dist != 0.0:
+                message = detectHandMoved_Z(dist)
 
             ## 結果を通知
+            sendMessageToServer(pipe_handle, message)
 
             ## OpenCVで表示
             cv2.imshow("window", removed)
@@ -328,6 +358,7 @@ def main():
 
     finally:
         pipeline.stop()
+        win32file.CloseHandle(pipe_handle)
 
 if __name__ == "__main__":
     main()
---
+++ optical_flow2.py
@@ -0,0 +1,71 @@
+# coding:utf-8
+## optical_flow.py
+## オプティカルフローのサンプルその２
+
+from __future__ import print_function
+import sys
+import numpy as np
+import pyrealsense2 as rs
+import cv2
+
+## パイプラインの用意
+pipeline = rs.pipeline()
+
+## キャプチャするフレームの設定
+config = rs.config()
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+## ストリーミングの開始
+profile = pipeline.start(config)
+
+saved_frame = None
+flag = True
+
+try:
+    while True:
+        ## 新規フレームを待機
+        frames = pipeline.wait_for_frames()
+
+        ## デプスフレームをRGBフレームに合わせる
+        align = rs.align(rs.stream.color)
+        aligned_frames = align.process(frames)
+
+        ## 各種フレームを取得
+        color = aligned_frames.get_color_frame()
+        depth = aligned_frames.get_depth_frame()
+
+        if not depth or not color: continue
+
+        ## キャプチャ画像をnumpy配列に変換
+        color_frame = np.asanyarray(color.get_data())
+        depth_frame = np.asanyarray(depth.get_data())
+
+        gray_img = cv2.cvtColor(color_frame, cv2.COLOR_BGR2GRAY)
+
+        if flag == True:
+            hsv = np.zeros_like(color_frame)
+            hsv[...,1] = 255
+
+        ## オプティカルフローの計算
+        if flag == False:
+            flow = cv2.calcOpticalFlowFarneback(saved_frame, gray_img, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+            hsv[...,0] = ang*180/np.pi/2
+            hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+            rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+
+            cv2.imshow("flow", rgb)
+
+        ## キャプチャしたフレームを一時的に保持
+        if flag == True:
+            saved_frame = gray_img
+            flag = False
+
+        ## OpenCVで表示
+        cv2.imshow("color", saved_frame)
+        key = cv2.waitKey(1)
+        if (key == 113): break
+
+finally:
+    pipeline.stop()
